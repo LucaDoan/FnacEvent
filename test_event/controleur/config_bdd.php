757bfdb5f5d99cebabe6812650947379e931e84a
LucaDoan <doanluca.an@gmail.com>
2023-06-19 14:59:35 +0200

    MAJ

--- test_event/controleur/config_bdd.php
+++ test_event/controleur/config_bdd.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-	$serveur="172.20.0.180";
-=======
-	$serveur="localhost:3306";
->>>>>>> cfb1638297c53a4a5fa6a20517de2a9d0044ed7e
+	$serveur="172.20.0.181";
 	$bdd="event";
 	$user="root";
 	$mdp="btssio2023";
